b85ea8ff575671580226d2688abc9c55f0f63af4
wolf-by <wolf.by@yandex.ru>
2024-01-22 16:56:33 +0300

    Add form to cart

--- app.rb
+++ app.rb
@@ -25,8 +25,8 @@ get '/catalog' do
 end
 
 post '/cart' do
-	orders_input = params[:orders]
-	@items = parse_orders_input orders_input
+	@orders_input = params[:orders]
+	@items = parse_orders_input @orders_input
 
 	@items.each do |item|
 		# id, cnt
